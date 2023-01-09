f4f1d38cb12818f4c68e9ad59cc87ada0f3953ab
Aly Samy Rady <alysamyrady@gmail.com>
2023-01-09 07:14:02 +0200

    connection DB

--- Databaseconnection/DataBaseConection.php
+++ Databaseconnection/DataBaseConection.php
@@ -1,11 +1,25 @@
 <?php
+   class DB{
 
-    include('./env.php');
-            try {
-                $conn=new PDO(DataBase,'host' . DBHost .';dbname=' . DBName ,UserName,userPassword);
-            }catch(PDOException $e ){
-                echo 'connection error' . $e->getMessage();
-            }
+    public $con;
+
+  
+   
+    function __construct() {
+        
+        include('./env.php');
+                try {
+                   $this->con=new PDO(DataBase . ":" . 'host='  . DBHost .';dbname=' . DBName ,UserName,userPassword);
+                }catch(PDOException $e ){
+                    echo 'connection error' . $e->getMessage();
+                }
+      }
+
+  }
+
+
+
+
             
        
 
--- Databaseconnection/env.php
+++ Databaseconnection/env.php
@@ -1,6 +1,6 @@
 <?php
 const DataBase='mysql';
 const DBHost='localhost';
-const DBName='users';
-const UserName='root';
+const DBName='cafeteria';
+const UserName='root'; 
 const userPassword='0000';
