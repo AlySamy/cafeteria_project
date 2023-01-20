2e33c3b25e77d27b5b0a764b6c0d453a7958d794
alaanassar <alaanassar1211@gmail.com>
2023-01-20 22:54:28 +0200

    update

--- php/dbclasses.php
+++ php/dbclasses.php
@@ -6,23 +6,54 @@ class DB
     protected $con;
     public function __construct($con){
         $this->con = $con;
-        var_dump($this->con);
+        // var_dump($this->con);
     }
-    // get all users
-    public function index($tableName)
+   
+     // get all users
+     public function index($tableName)
+     {
+         try{
+             $query = "SELECT * FROM $tableName where is_admin = 0";
+             $sql = $this->con->prepare($query);
+             $sql->execute();
+             $data = $sql->fetchAll(PDO::FETCH_ASSOC);
+             return $data;
+         }catch (PDOException $e) {
+             echo "Error: ".$e->getMessage();
+         }
+         
+     }
+    // get users room 
+    public function usersroom($tableName1,$tableName2,$index)
     {
         try{
-            $query = "SELECT * FROM $tableName";
+            // $query = "SELECT * FROM $tableName1 INNER JOIN $tableName2 on $tableName1.id =$tableName2.user_id LIMIT $index,4";
+            $query = "SELECT $tableName2.*, $tableName1.*   FROM $tableName1, $tableName2 where $tableName1.id = $tableName2.user_id LIMIT $index,4";
             $sql = $this->con->prepare($query);
             $sql->execute();
             $data = $sql->fetchAll(PDO::FETCH_ASSOC);
-            var_dump($data);
+            
             return $data;
         }catch (PDOException $e) {
             echo "Error: ".$e->getMessage();
         }
         
     }
+
+    // public function testPagination($tableName,$index)
+    // {
+    //     try{
+    //         $query = "SELECT * FROM $tableName WHERE ";
+    //         $sql = $this->con->prepare($query);
+    //         $sql->execute();
+    //         $data = $sql->fetchAll(PDO::FETCH_ASSOC);
+    //         return $data;
+    //     }catch (PDOException $e) {
+    //         echo "Error: ".$e->getMessage();
+    //     }
+    // }
+    
+      
     // get single user by id
     public function show($tableName,$id)
     {
@@ -31,29 +62,50 @@ class DB
             $sql = $this->con->prepare($query);
             $sql->execute();
             $data = $sql->fetch(PDO::FETCH_ASSOC);
-            var_dump($data);
+            // var_dump($data);
             return $data;
         }catch (PDOException $e) {
             echo "Error: ".$e->getMessage();
         }
     }
-    // edit user
-    public function update($tableName,$id,$data)
+    // update user
+    public function updateUser($tableName,$tableRoom,$id,$name,$email,$room,$img)
     {
         try{
-            $columns='';
-            foreach($data as $key=> $value)
-            {
-                $columns=$columns.$key."="."'".$value."'".",";
-            }
-            $columns=rtrim($columns,",");
-            $query="UPDATE $tableName SET $columns WHERE id=$id";
+            $query="UPDATE $tableName SET name='$name', email='$email',profile_pic='$img' WHERE id=$id";
+            $queryRoom="UPDATE $tableRoom SET Room_number = $room WHERE user_id=$id";
             $sql=$this->con->prepare($query);
             $sql->execute();
+            $sql2=$this->con->prepare($queryRoom);
+            $sql2->execute();   
         }catch (PDOException $e) {
             echo "Error: ".$e->getMessage();
         }
     }
+    // validate form
+    public function validateUser($id,$email,$room)
+    {
+        try{
+            // SELECT * FROM $tableName1 INNER JOIN $tableName2 on $tableName1.id =$tableName2.user_id
+            $query = "SELECT email,Room_number FROM users INNER JOIN user_room on  email='$email' and Room_number =$room ";
+            // $query="SELECT email, Room_number from users , user_room where users.id = user_room.user_id and   email = '$email' and Room_number = $room";
+            $sql = $this->con->prepare($query);
+            $sql->execute();
+            $data = $sql->fetchAll(PDO::FETCH_ASSOC);
+            // var_dump($data);
+            // if($data){
+            //     return false;
+
+            // }else{
+            //    return true;
+            // }
+            
+            return $data;   
+        }catch (PDOException $e) {
+            echo "Error: ".$e->getMessage();
+        }
+    }
+
     // create user
     public function store($tableName,$data)
     {
@@ -61,7 +113,7 @@ class DB
             $query = "INSERT INTO ".$tableName." (";            
             $query .= implode(",", array_keys($data)) . ') VALUES (';            
             $query .= "'" . implode("','", array_values($data)) . "')";  
-            var_dump($query);
+            // var_dump($query);
             $sql = $this->con->prepare($query);
             $sql->execute();
         }catch (PDOException $e) {
@@ -79,10 +131,26 @@ class DB
             echo "Error: ".$e->getMessage();
         }
     }
+    // check user
+    public function checkUser($tableName, $email )
+    {
+        $query = "SELECT is_admin from $tableName where email='$email'";
+        $sql = $this->con->prepare($query);
+        $sql->execute();
+        $data = $sql->fetch(PDO::FETCH_ASSOC);
+        
+        return $data;
+    }
+
+    
 }
 
-$db = new DB($con);
-$db->index('users');
+// $db = new DB($con);
+// $x=$db->validateUser(6,"alaa1234@gmail.com",200);
+// var_dump($x);
+// $db->validateUser(6,"alaa@gmail.com",200);
+// $db->usersroom("users","user_room",8);
+// $db->index('users');
 // $db->show('users',1);
 // $db->store('users' , ['name'=>'ahmed','email'=>'ahmed@gmail.com', 'password'=>'12345678', 'profile_pic'=>'./images/0.12204800 1672674506.jpeg']);
 // $db->store('users' , ['name'=>'ali','email'=>'ali@gmail.com', 'password'=>'12345678', 'profile_pic'=>'./images/0.12204800 1672674506.jpeg']);
---
+++ php/updateOneUser.php
@@ -0,0 +1,23 @@
+<?php
+require("./dbclasses.php");
+$db=new DB($con);
+$id=$_REQUEST["id"];
+$email=$_REQUEST["email"];
+$name=$_REQUEST["name"];
+$room=$_REQUEST["room"];
+$img=$_REQUEST["imageName"];
+
+$data=$db->validateUser($id,$email,$room);
+if($data){
+    echo "error";
+    setcookie('errors', json_encode(['Email' => 'Not valid']),0,'/');
+}else{
+    
+     $data = $db->updateUser('users','user_room',$id,$name,$email,$room,$img);
+    header("Location: ../all_users.html");
+    setcookie('errors','', -1, '/');
+
+}
+// echo json_encode($data);
+
+
