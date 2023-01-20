acbf389e11c1f9403e5ada57083ea65aab79d6b4
Aly Samy Rady <alysamyrady@gmail.com>
2023-01-20 03:28:19 +0200

    add session

--- js/session.js
+++ js/session.js
@@ -1,12 +1,16 @@
 async function getUserData() {
   let result = await fetch("./php/chec_session.php");
-  let data = await result.jsop();
+  let data = await result.json();
   manpulateResponse(data);
 }
 function manpulateResponse(data) {
-  data.forEach((obj) => {
-    addUserData(obj);
-  });
+  if (data.login == "notValid") {
+    window.open("./login page", "_self");
+  } else {
+    data.forEach((obj) => {
+      addUserData(obj);
+    });
+  }
 }
 
 function addUserData(obj) {
@@ -14,4 +18,21 @@ function addUserData(obj) {
   userName.innerHTML = obj.name;
   let userPic = document.getElementById("img");
   userPic.src = `${obj.product_pic}`;
+}
+
+let logOutBtn = document.getAnimations("log_out_btn");
+logOutBtn.addEventListener("click", () => {
+  userLogOut();
+});
+
+async function userLogOut() {
+  let result = await fetch("./php/log_out.php");
+  let data = await result.json();
+  manpulateuserData(data);
+}
+
+function manpulateuserData(user) {
+  if (user.logout == valid) {
+    window.open("./login page", "_self");
+  }
 }
