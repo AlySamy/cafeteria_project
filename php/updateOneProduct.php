7434b2828f654dc15b7d9db08c7aa6c27878282c
Aly Samy Rady <alysamyrady@gmail.com>
2023-01-26 17:38:20 +0200

    edit

--- php/updateOneProduct.php
+++ php/updateOneProduct.php
@@ -6,7 +6,7 @@ $productprice=$_REQUEST['price'];
 $productStatus=$_REQUEST['status'];
 $productId=$_REQUEST['id'];
 $catagoryId=$_REQUEST['category'];
-$categoryPic=$_FILES['img'];
+$categoryPic=$_REQUEST['img'];
 
 
 
@@ -20,7 +20,7 @@ foreach ($_REQUEST as $key => $value) {
 }
 if (!empty($errors)) {
     setcookie("errors", json_encode($errors),0,'/');
-    // header("location:../all_product.html");
+    header("location:../all_product.html");
     exit();
 } else {
     
@@ -29,42 +29,42 @@ setcookie('errors','', -1, '/');
 }
 
 
-$imgSize = $categoryPic['size'];
-if ($imgSize > 2000000) {
-    setcookie('errors', json_encode(['image' => 'size of image bigger than 2MG']),0,'/');
-    header("location:../all_product.html");
-    exit();
-} else {
-    setcookie('errors','', -1, '/');
-}
+// $imgSize = $categoryPic['size'];
+// if ($imgSize > 2000000) {
+//     setcookie('errors', json_encode(['image' => 'size of image bigger than 2MG']),0,'/');
+//     header("location:../all_product.html");
+//     exit();
+// } else {
+//     setcookie('errors','', -1, '/');
+// }
 
-$allowed_image_extension = array('png', 'jpg', 'jpeg');
-$imgExtension = explode('/', $categoryPic['type'])[1];
-if (!in_array($imgExtension, $allowed_image_extension)) {
-    setcookie("errors", json_encode(['image' => 'extension image is not valid']),0,'/');
-    header("location:../all_product.html");
-    exit();
-} else {
-    setcookie('errors','', -1, '/');
-}
- $categoryPic=$_FILES['img'];
-$file_path = $categoryPic['tmp_name'];
-$categoryPic = '../images/products/' . time() . '.' . explode('/', mime_content_type($file_path))[1];
-move_uploaded_file($file_path, $categoryPic);
+// $allowed_image_extension = array('png', 'jpg', 'jpeg');
+// $imgExtension = explode('/', $categoryPic['type'])[1];
+// if (!in_array($imgExtension, $allowed_image_extension)) {
+//     setcookie("errors", json_encode(['image' => 'extension image is not valid']),0,'/');
+//     header("location:../all_product.html");
+//     exit();
+// } else {
+//     setcookie('errors','', -1, '/');
+// }
+//  $categoryPic=$_FILES['img'];
+// $file_path = $categoryPic['tmp_name'];
+// $categoryPic = '../images/products/' . time() . '.' . explode('/', mime_content_type($file_path))[1];
+// move_uploaded_file($file_path, $categoryPic);
 
-$catagorypicuplode= time() . '.' . $imgExtension;
+// $catagorypicuplode= time() . '.' . $imgExtension;
 
 
 $result=$db->getOneProduct('product',$productName);
 
 if($result){
-    $db->updateProuductEpxeptName($productId,$productprice,$catagorypicuplode,$productStatus,$catagoryId);
+    $db->updateProuductEpxeptName($productId,$productprice,$categoryPic,$productStatus,$catagoryId);
     setcookie("errors", json_encode(['errors'=>'data exist']),0,'/');
     header('Location:../all_product.html');
 
 }else{
     
-    $db->udateproductData($productId,$productName,$productprice,$catagorypicuplode,$productStatus,$catagoryId);
+    $db->udateproductData($productId,$productName,$productprice,$categoryPic,$productStatus,$catagoryId);
     header('Location:../all_product.html');
     setcookie('errors','', -1, '/');
 }
